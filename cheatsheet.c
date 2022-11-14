1f267401e1d44d62a744510b279dd1cdc2854856
RobertJesus153 <robert.j.paneque@gmail.com>
2022-11-14 23:55:21 +0100

    Added personal cheatsheet

---
+++ cheatsheet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+    int integer;
+    float decimal;
+    char a;
+    short short_integer;
+    long long_integer;
+    double double_precission_floating;
+
+    scanf("Input: ");
+    printf("%d\t%f\b\n", integer, decimal);
+    printf("%3d %f\n", 5/9, 5.0/9.0);
+    printf("%o\t%x\n", 16, 0x6a);
+    printf("%c\t%s\n", 'a', "text");
+
+}
+
