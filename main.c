9dfce1da7b83d70fd05c454f2d3ff73bdf00b345
RobertJesus153 <robert.j.paneque@gmail.com>
2022-11-29 21:57:48 +0100

    Monte Carlo simulation for obtaining pi

--- main.c
+++ main.c
@@ -2,13 +2,40 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define SEED time(0)
+#define NITER 1e1
+#define DIM 2       // Dimension = 2 (circle)
 
-long random_at_most(long max) {
-    /* Code by Ryan Reich: 
-    * https://stackoverflow.com/questions/2509679/how-to-generate-a-random-integer-number-from-within-a-range
-    */ 
-    
+int main(){
+    double x[DIM];
+    double *px = &x;
+    double pi;
+    long seed = time(0);
+
+    // Display title and seed for random numbers
+    printf("####### MONTE CARLO SIMULATION: PI #######\n");
+    printf("seed: %ld\n", seed);
+
+    // Use seed for random numbers
+    srand(seed);
+
+    // Monte Carlo
+    for (unsigned long int i=0; i<NITER; i++) {
+        for (int k=0; k<DIM; k++) {
+            x[k] = (double) rand()/RAND_MAX * 2 - 1;
+        }
+        printf("%8.3f %8.3f \n", x[0], x[1]);
+    }
+
+
+    return 0;
+}
+
+/*
+ long random_at_most(long max) {
+    // Code by Ryan Reich:
+    // https://stackoverflow.com/questions/2509679/how-to-generate-a-random-integer-number-from-within-a-range
+    //
+
     // Assumes 0 <= max <= RAND_MAX
     // Returns in the closed interval [0, max]
     unsigned long
@@ -22,12 +49,9 @@ long random_at_most(long max) {
     do {
         x = random();        // random() has better distribution than rand(), not available in Windows
         printf("%ld ", x);
-    }  
+    }
     while (num_rand - defect <= (unsigned long) x);        // This is carefully written not to overflow
 
     return x/bin_size;        // Truncated division is intentional
 }
-int main(){
-    printf("Monte Carlo simulation");
-    return 0;
-}
+ */
