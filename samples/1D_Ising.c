b903a7f338af0ef0ace6328edbe6c0e9d9608e91
RobertJesus153 <robert.j.paneque@gmail.com>
2022-12-05 22:50:41 +0100

    Added 1D_Ising.c (scheme)

---
+++ samples/1D_Ising.c
@@ -0,0 +1,18 @@
+/* Estimate mean energy of a 1D system of spins
+ * using Ising model
+ * using Monte Carlo dynamic methods:
+ *                      metropolis
+ *                      heat bath
+*/
+
+#include<stdio.h>
+
+double *metropolis (float *beta, float *J, int N);
+
+double *heat_bath (float *beta, float *J, int N);
+
+
+int main (int argc, char *argv[]){
+
+    return 0;
+}
