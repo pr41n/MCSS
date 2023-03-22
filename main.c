7406212403f5bba900b75cf9ba22d073d05f177d
RobertJesus153 <robert.j.paneque@gmail.com>
2023-03-22 12:23:30 +0100

    implemented Wolff algorithm
    added block measures, thermalization and relaxation time

--- main.c
+++ main.c
@@ -1,258 +1,248 @@
-/* Estimated mean energy and magnetization of a 2D system of spins
- * using Ising model
- * using Monte Carlo dynamic methods:
- *                      metropolis
- *                      heat bath
-*/
-
-/*Differences with 1D_Ising: N, for_loop, lattice_def - in main
-                             ener_sum, magn_sum,
-                             metropolis_update (L -> L*L)
-*/
+/*Energy and magnetization for a 2D-Ising periodical
+  lattice using Wolff algorithm*/
 
 #include<stdio.h>
 #include<stdlib.h>
+#include<stdint.h>
 #include<math.h>
 #include<time.h>
 
-int metropolis_update (float kappa, int L, int *lattice, int x);
-int heat_bath (float *beta, float *J, int N);
+/*----VARIABLES----*/
+/*-----------------*/
+/*--Pre-definitions--*/
+#define NBLCKDISP 10     // max number of blocks to display
 
-double ener_sum (int L, int *lat);
-double magn_sum (int L, int *lat);
+/*--Global variables--*/
+/*Lattice*/
+int16_t L;                 // length of the lattice (up to 2^14)
+uint32_t N;                 // number of particles (L*L)
+uint8_t  shift=1;           // log_2(L), at least 1
 
-int *stagger_lattice (int L);
-int *up_lattice (int L);
-int *down_lattice (int L);
-int *random_lattice (int L);
+int8_t *lat;                    // lattice (possible elements -1, +1, 0, -2)
+int16_t *rn, *ln, *un, *dn,     // relative positions of neighbours (+1, -1, +L, -L)
+        nn[4];                  // array for storing them
+uint32_t n;                     // neighbour (0,1,2,...,N)
 
-void disp_lattice(int L, int *lat);
+/*PRNG*/
+uint64_t rnd,           // PRNG
+         prob_bond;     // probability 1-exp(-2k)
+float    frnd;          // normalised rnd 0<frnd<=1 (for Wolff)
 
-////////// MAIN
-int main (int argc, char *argv[]){
-    // TODO optional lattice as input (txt)
-    // TODO input args using commands (-beta, -J, -o)
+/*Physical values*/
+float   kappa;          // constant J/kT
+double  e,              // observable energy density
+        m,              // observable magnetization density
+        m2;             // observable squared magnetization density
 
-    /*INPUT*/
-    int L;
-    float J, beta, niter;
+/*Simulation*/
+int8_t spin;            // value of the selected spin from the cluster
 
-    if (argc == 1 || argc > 6){
-        printf("Usage:\t %s L output_file [OPTIONAL] niter J beta \n", argv[0]);
-        printf("\tdefault: niter=1E+03, J=1, beta=0.44\n");
+uint8_t  nblock,        // number of blocks
+         nmeas,         // number of measures per block
+         nupdte,        // number of updates between 2 measures (from relaxation time)
+         ntherm;        // number of thermalization updates
+uint32_t ntotal,        // total number of updates
+         Ncs,           // number of spins inside the cluster
+         Nc;            // number of clusters (for SW)
+
+/*External files*/
+FILE    //*in_file,        // input file
+        //*bk_file,        // back up file
+        *out_file;       // output file
+
+/*--Predefinitions for better performance--*/
+/*iterators*/
+int16_t x, y;        // coords in lattice
+uint32_t i,          // current site in lattice (x+L*y)
+         j, k;       // multi-purpose iterators
+uint8_t  nit;        // iterator for neighbours (up to 4)
+/*precalcs*/
+double invN,         // 1/N
+       JinvN;        // J/N
+
+
+/*----FUNCTIONS----*/
+/*-----------------*/
+/*--PRNG--*/
+void xorshift64(){           // generates a PRNG in (0, 2^64-1]
+    rnd ^= rnd << 13;
+    rnd ^= rnd >> 7;
+    rnd ^= rnd << 17;
+}
+/*--init--*/
+void get_data(int argc, char *argv[]){
+    /*Get data from input (by now exec args, TODO entry file)*/
+    if (argc == 1 || argc > 7){
+        printf("Usage:\t %s L output_file [OPTIONAL] nblock nmeas nupdte ntherm kappa\n", argv[0]);
+        printf("default: nblock=20, nmeas=1000, nmeas=5, ntherm=10, kappa=0.4406868\n");
         exit(1);
     }
 
-    sscanf(argv[1], "%d", &L);
-    FILE *file = fopen(argv[2], "w");
-    sscanf((argc>=4) ? argv[3] : "1e3", "%f", &niter);        // set input if it exists else default value
-    sscanf((argc>=5) ? argv[4] : "1", "%f", &J);
-    sscanf((argc==6) ? argv[5] : "0.44", "%f", &beta);
-
-    if (L%2 != 0){printf("ERROR: L must be even\n"); exit(1);}
-    if (L<2){printf("ERROR: L must be >=2\n"); exit(1);}
-    if (beta<=0){printf("ERROR: beta must be positive\n"); exit(1);}
+    sscanf(argv[1], "%hd", &L);
+    out_file = fopen(argv[2], "w");
+    sscanf((argc>=4) ? argv[3] : "20", "%hhu", &nblock);        // set input if it exists elne default value
+    sscanf((argc>=5) ? argv[4] : "1000", "%hhu", &nmeas);
+    sscanf((argc>=6) ? argv[5] : "5", "%hhu", &nupdte);
+    sscanf((argc>=7) ? argv[6] : "10", "%hhu", &ntherm);
+    sscanf((argc>=8) ? argv[7] : "0.4406868", "%f", &kappa);
 
 
-    // BEGIN
-    int N = L*L;
-    float invN = (float) 1/N;
-    float kappa = J*beta;
+    if (L<=1 || (L&(L-1))){printf("ERROR: L must be 2^n with n>0\n"); exit(1);}
+    if (kappa<=0){printf("ERROR: kappa (=J*beta with J=1) must be positive\n"); exit(1);}
+}
 
-    int ndisp = (int) niter/10,
-        nstore = (int) niter/1e7;
-
-    if (ndisp==0)  ndisp=1;
-    if (nstore==0) nstore=1;
-
-    srand(time(0));
-
-    // Disp info
-    printf("iterations: %3.2g\nparticles: %d\nbeta*J = %f\t(", niter, N, kappa);
-    if (kappa - log(1+sqrt(2))/2 < 1e-6) printf("= ");
-    else if (kappa < log(1+sqrt(2))/2) printf("< ");
-    else printf("> ");
-    printf("log(1+sqrt(2))/2 = 0.440687..)\n\n");
-
-    // Generating initial lattice
-    int *lattice = random_lattice(L);          // May test with stagger_lattice, up_lattice, down_lattice
-    double e = -J * ener_sum(L, lattice) * invN;
-    double m = magn_sum(L, lattice) * invN;
-
-    //printf("Initial configuration:\te=%4.3f\tm=%4.3f\n", e, m);
-    //disp_lattice(L, lattice);
-    //puts("\n");
-
-    // Calculate energy and magnetization for beta
-    // Sequential update
-    for (int n=0; n<niter; n++){
-        if (n%nstore == 0) {
-            e = -J * ener_sum(L, lattice) * invN;
-            m = magn_sum(L, lattice) * invN;
-            fprintf(file, "\n%.8f\t%.8f", e, m);
-        }
-        if (n%ndisp == 0) {
-            e = -J * ener_sum(L, lattice) * invN;
-            m = magn_sum(L, lattice) * invN;
-            printf("%3.0f%%:\te=%4.3f\tm=%4.3f\n", (float) n/niter*100, e, m);
-            disp_lattice(L, lattice);
-        }
-
-        for (int k=0; k<N; k++){
-            if (metropolis_update(kappa, L, lattice, k))
-                lattice[k] = -lattice[k];
-        }
+void setup(){
+    /*--Remaining information--*/
+    N = L*L;
+    while (L>>(shift+1)){
+        shift++;
     }
-    e = -J * ener_sum(L, lattice) * invN;
-    m = magn_sum(L, lattice) * invN;
-    printf("100%%:\te=%4.3f\tm=%4.3f\n", e, m);
-    disp_lattice(L, lattice);
+    ntotal = ntherm + nblock*nmeas*nupdte;
 
-    return 0;
-}
-//////////
+    /*--Predefinitions--*/
+    int J=1;        //supposing J=1, else it should be input
+    invN = (double) 1/N;
+    JinvN = (double) J/N;
 
-double ener_sum (int L, int *lat) {
-    /* E = J * sum(s_i*s_j)
-     * sum through the closest neighbours of each element of lattice
-     * considering only right and up neighbour (for not repeating)
-     * assuming periodical lattice
-     */
-    double E = 0;
-    /*Forward sum*/
-    for (int i=0; i<L-1; i++){
-    for (int j=0; j<L-1; j++)
-        E += lat[i*L + j] * (lat[i*L + j+1] + lat[(i+1)*L + j]);
-    E += lat[i*L + L-1] * (lat[i*L + 0] + lat[(i+1)*L + L-1]);
+    /*--PRNG--*/
+    prob_bond = (uint64_t) (exp(-2*kappa) * 0x1p+64);   // more precise than 1-exp(-2k)
+    rnd = (uint64_t) time(0);
+
+    /*--Lattice--*/
+    lat= (int8_t *) malloc(sizeof(char) * N);       // realocating memory
+    rn = (int16_t*) malloc(sizeof(short) * L);
+    ln = (int16_t*) malloc(sizeof(short) * L);
+    un = (int16_t*) malloc(sizeof(short) * L);
+    dn = (int16_t*) malloc(sizeof(short) * L);
+
+    for (i=0; i<N; i++){                            // set all spins to +1 or randomly
+        xorshift64();                               // some updates to the random number
+        lat[i] = +1;  //(rnd&1) * 2 - 1;
     }
-    for (int j=0; j<L-1; j++)
-        E += lat[(L-1)*L + j] * (lat[(L-1)*L + j+1] + lat[0 + j]);
-    E += lat[(L-1)*L + L-1] * (lat[0 + L-1] + lat[(L-1)*L + L-2]);
-
-    /* Backward sum
-
-    */
-    return E;
-}
-
-double magn_sum (int L, int *lat) {
-    /* M = sum(s_i)
-     * sum of each element of lattice
-     * assuming periodical lattice
-     */
-    double M = 0;
-    for (int i=0; i<L; i++){
-    for (int j=0; j<L; j++)
-        M += lat[i*L + j];
+    for (i=0; i<L; i++){                            // set relative neighbors
+        rn[i] = +1;     // right neighbour
+        ln[i] = -1;     // left neighbour
+        un[i] = L;      // up neighbour
+        dn[i] = -L;     // down neighbour
     }
-    return M;
+    /*boundry conditions*/
+    ln[0] = L-1;
+    rn[L-1] = -ln[0];
+    dn[0] = (L-1)*L;
+    un[L-1] = -dn[0];
 }
-
-int metropolis_update (float kappa, int L, int *lattice, int x){
-    /* Dynamic MC-method: Metropolis
-     * returns boolean value whether to change or not
+/*--MC update--*/
+void expand_cluster(uint16_t x_cx, uint16_t y_cx, uint32_t i_cx){
+    /* we need to create new cluster expansion versions of x,y,i,n,nn variables
+     * which will be in different memory locations, one for each function call
      */
-    int *lat0 = lattice;
+    uint8_t nit_cx;
+    int16_t nn_cx[4] = {rn[x_cx], ln[x_cx],
+                        un[y_cx], dn[y_cx]};
+    uint32_t n_cx;
 
-    /*-------Generally-------*//*
-    int lat1[L*L];
-
-    for (int k=0; k<L*L; k++)
-        lat1[k] = (k!=x) ? lat0[k] : -lat0[k];
-
-    double  P0 = exp(kappa * ener_sum(L, lat0)),
-            P1 = exp(kappa * ener_sum(L, lat1));
-
-    double Q = P1/P0;*/
-    /*-------Specifically-------*/
-    int s_up =    (x >= L*(L-1)) ? lat0[x%L]       : lat0[x+L],
-        s_down =  (x <= L-1)     ? lat0[x+L*(L-1)] : lat0[x-L],
-        s_left =  (x%L == 0)     ? lat0[x+L-1]     : lat0[x-1],
-        s_right = (x%L == L-1)   ? lat0[x-L+1]     : lat0[x+1];
-
-    double diff = -2*lat0[x] * (s_up + s_down + s_left + s_right);
-    double Q = exp(kappa * diff);
-
-    /*-------Always-------*/
-    if (Q >= 1) return 1;
-
-    double r = (double) rand()/RAND_MAX;        // random number in [0, 1]      TODO should be in (0,1)
-    if (r < Q) return 1;
-    else return 0;
-}
-
-int *stagger_lattice (int L){
-    /* stagger lattice (+ - + - + - ...
-     *                  - + - + - + ...
-     *                  ...)
-     */
-    int *lat = NULL;
-    lat = realloc(lat, sizeof(int) * L*L);
-
-    for (int i=0; i<L; i++){
-    for (int j=0; j<L; j++)
-        lat[i*L + j] = pow(-1, i) * pow(-1, j%2);
-    }
-
-    return lat;
-}
-
-int *up_lattice (int L){
-    /* spins up lattice (+ + + + + + ...
-    *                    + + + + + + ...
-    *                    ...)
-     */
-    int *lat = NULL;
-    lat = realloc(lat, sizeof(int) * L*L);
-
-    for (int i=0; i<L; i++){
-    for (int j=0; j<L; j++)
-        lat[i*L + j] = 1;
-    }
-
-    return lat;
-}
-
-int *down_lattice (int L){
-    /* spins down lattice (- - - - - - ...
-    *                      - - - - - - ...
-    *                      ...)
-     */
-    int *lat = NULL;
-    lat = realloc(lat, sizeof(int) * L*L);
-
-    for (int i=0; i<L; i++){
-    for (int j=0; j<L; j++)
-        lat[i*L + j] = -1;
-    }
-
-    return lat;
-}
-
-int *random_lattice (int L){
-    /* random lattice (+ - + + + - ...
-     *                 - + - - + - ...
-     *                 ...)
-     */
-    int r;          // random spin obtained each iteration (+1 or -1)
-    int *lat = NULL;
-    lat = realloc(lat, sizeof(int) * L*L);
-
-    for (int i=0; i<L; i++) {
-        for (int j=0; j<L; j++){
-            r = (rand()%2) * 2 - 1;     // +1 or -1 with equal probability
-            lat[i*L + j] = r;
+    for (nit_cx=0; nit_cx<4; nit_cx++){         // check all neighbours
+        n_cx = i_cx + nn_cx[nit_cx];
+        if (lat[n_cx] == spin){         // could be inside the cluster
+            xorshift64();
+            if (rnd > prob_bond){    // it is!
+                Ncs++;
+                lat[n_cx] = -spin;
+                expand_cluster(n_cx&(L-1), n_cx>>shift ,n_cx);   // now expand the cluster from it
+            }
         }
     }
-    return lat;
 }
+void Wolff(){
+    xorshift64();                   // choose randomly the spin for the new cluster in (0,N)
+    frnd = (float) rnd * 0x1p-64;
+    i = (uint32_t) (frnd * N);
 
-void disp_lattice(int L, int *lat) {
-    for (int i=0; i<L; i++){
-        for (int j=0; j<L; j++){
-            if (lat[i*L+j]==1) printf("+");
+    spin = lat[i];      // save spin value for expansion
+    lat[i] = -spin;     // flip the spin
+    expand_cluster(i&(L-1), i>>shift, i);  // expand the cluster
+}
+/*--measurements--*/
+void measure() {
+    static int64_t E, M;    // should be a 64-bit integer since N is 32-bit unsigned integer
+
+    E = M = i = 0;      // reset measurements and site
+
+    for (y=0; y<L; y++) {
+        for (x=0; x<L; x++){
+            spin = lat[i];
+            M += spin;
+
+            nn[0] = rn[x];
+            nn[1] = un[y];
+            for (nit=0; nit<2; nit++){
+                n = i + nn[nit];
+                E += spin * lat[n];
+            }
+            i++;
+        }
+    }
+    e  = (double) -JinvN * E;
+    m  = (double) invN * M;
+    m2 = (double) invN * Ncs*Ncs;           // <M²> = \sum_{clusters} <Ncs> and just one cluster for Wolff
+}
+/*--output--*/
+void disp_lattice(int8_t *lattice) {
+    for (j=0; j<N; j++){
+        if ((j & (L-1)) == 0) puts("");     // new line
+        if (lattice[j]&1){
+            if (lattice[j]==1) printf("+");
             else printf("-");
         }
-    puts("");
+        else {
+            if (lattice[j]==0) printf("o");
+            else printf(".");
+        }
     }
+    puts("");
+}
+void disp_init_info() {
+    printf("total steps: %d\nthermalization steps: %d\nmeasures: %d\nparticles: %d\nkappa: %.7f ", ntotal, ntherm, nblock*nmeas, N, kappa);
+    if (kappa - log(1+sqrt(2))/2 < 1e-10)    printf("(near critical point ");
+    else if (kappa < log(1+sqrt(2))/2)      printf("(below critical point ");
+    else                                    printf("(above critical point ");
+    printf("log(1+sqrt(2))/2 = 0.4406868..)\n\n");
+}
+
+/*----MAIN PROGRAM----*/
+/*-----------------*/
+int main(int argc, char *argv[]){
+    get_data(argc, argv);       // Getting input data
+    setup();                    // Setting up and generating the lattice
+
+    uint8_t nbdisp = nblock/NBLCKDISP;
+    if (nbdisp == 0) nbdisp = 1;        // nblock < NBLCKDISP, so display them all
+
+    disp_init_info();               // disp some initial info
+
+    /*Thermalization*/
+    printf("Beginning thermalization\n");
+    for (int nt=0; nt<ntherm; nt++) {
+        Wolff();
+    }
+    //disp_lattice(lat);
+    printf("Thermalization finished!\n\n");
+
+    /*Measurements*/
+    printf("Beginning measures\n");
+    for (int nb=0; nb<nblock; nb++) {
+        if (nb%nbdisp == 0 || nb == nblock-1){
+            measure();
+            printf("%3.0f%%:\te=%4.3f\tm=%4.3f\n", (float) nb/nblock*100, e, m);
+            //disp_lattice(lat);
+        }
+        for (j=0; j<nmeas; j++){
+            for (k=0; k<nupdte; k++)
+                Wolff();
+            measure();
+            fprintf(out_file, "\n%6.4f\t%6.4f\t%6.4f", e, m, m2);
+        }
+    }
+    printf("Measures finished!\n");
+    return 0;
 }
